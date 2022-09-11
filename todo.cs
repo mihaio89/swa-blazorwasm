17c43db63127571dd1e3d0bd5d151d557fc0c56a
Mihai Olaru <molaru@microsoft.com>
2022-09-11 14:45:14 -0700

    new to do page

---
+++ todo.cs
@@ -0,0 +1,9 @@
+public class todo
+{
+    public string? title { get; set; }
+    public string? details { get; set; }
+
+    //public Uri? url { get; set; }
+    public string? url { get; set; }
+    public bool done { get; set; }
+}
